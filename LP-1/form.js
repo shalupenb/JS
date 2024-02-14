7bb014cdf42dce09f787cdb051e8ee51664b9507
Danya Gichak <danyacorporation6@gmail.com>
2024-02-14 15:46:18 +0200

    commit

--- LP-1/form.js
+++ LP-1/form.js
@@ -1,32 +1,57 @@
-document.onsubmit = function(e)
-{
-  e.preventDefault();
-  const userNameInput = e.target.querySelector('[name="user-name"]');
-  if(!userNameInput){
-    alert('Cant find user-name');
-    return;
-  }
-  const userName = userNameInput.value.trim();
-  if(userName.length == 0) {
-    userNameInput.parentNode.querySelector(".error-message").innerText = "Cant be empty";
-    userNameInput.classList.remove("valid");
-    userNameInput.classList.add("invalid");
-  }
-  else if(userName.length == 1) {
-    userNameInput.parentNode.querySelector(".error-message").innerText = "Too short";
-    userNameInput.classList.remove("valid");
-    userNameInput.classList.add("invalid");
-  }
-  else {
-    userNameInput.classList.add("valid");
-    userNameInput.classList.remove("invalid");
-  }
-  
-  const userPasswordInput = e.target.querySelector('[name="user-password"]');
-  if(!userPasswordInput){
-    alert('Cant find user-password');
-    return;
-  }
-  const userPassword = userPasswordInput.value;
-  console.log(userName + " : " + userPassword);
+document.onsubmit = function(e){
+    e.preventDefault();
+
+    const userNameInput = e.target.querySelector('[name="user-name"]');
+    if(!userNameInput){
+        alert('В форме не найден элемент [name="user-name"]');
+        return;
+    }
+    const userName = userNameInput.value.trim();
+    
+    if(userName.length == 0){
+        setHelperMessage(userNameInput, "Поле не должно быть пустым", true);
+    }
+    else if (userName.length == 1){
+        setHelperMessage(userNameInput, "Имя слишком короткое", true);
+    }
+    else{
+        setHelperMessage(userNameInput, "", false)
+    }
+    const userPassInput = e.target.querySelector('[name="user-password"]');
+    if(!userNameInput){
+        alert('В форме не найден элемент [name="user-password"]');
+        return;
+    }
+    const userPass = userPassInput.value.trim();
+    if(userPass.length == 0){
+        userPassInput.parentNode.querySelector(".error-message").innerText = "Поле не должно быть пустым"
+        userPassInput.classList.remove("valid");
+        userPassInput.classList.add("invalid");
+    }
+    else if (userPass.length < 3){
+        userPassInput.parentNode.querySelector(".error-message").innerText = "Пароль должен содержать больше 3 символов"
+        userPassInput.classList.remove("valid");
+        userPassInput.classList.add("invalid");
+    }
+    else{
+        userPassInput.classList.add("valid");
+        userPassInput.classList.remove("invalid");
+    }
+    console.log(userName + " : " + userPass);
+}
+
+function setHelperMessage(inputElement, message, isError){
+    const helper = inputElement.parentNode.querySelector(".helper-text");
+    if(isError){
+        helper.setAttribute("data-error", message)
+        inputElement.classList.remove("valid");
+        inputElement.classList.remove("validate");
+        inputElement.classList.add("invalid");
+    }
+    else{
+        helper.setAttribute("data-error", message)
+        inputElement.classList.add("valid");
+        inputElement.classList.add("validate");
+        inputElement.classList.remove("invalid");
+    }
 }
