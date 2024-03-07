ac165f167633ef6fc789fda6d69bed1e1683c807
Danya Gichak <danyacorporation6@gmail.com>
2024-03-07 05:55:53 +0200

    commit

---
+++ LP-1/canvas.js
@@ -0,0 +1,162 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  const figure = document.getElementById("figure");
+  if(!figure) throw "Element '#figure' not found";
+  const context = figure.getContext('2d');
+  window.figure = figure;
+  window.gdc = context;
+
+  const line1Button = document.getElementById("line-1-button");
+  if(!line1Button) throw "Element '#line-1-button' not found";
+  line1Button.onclick = drawLine1;
+  const line2Button = document.getElementById("line-2-button");
+  if(!line2Button) throw "Element '#line-2-button' not found";
+  line2Button.onclick = drawLine2;
+  const expandButton = document.getElementById("expand-button");
+  if(!expandButton) throw "Element '#expandButton' not found";
+  expandButton.onclick = expand;
+  const resolutionButton = document.getElementById("resolution-button");
+  if(!resolutionButton) throw "Element '#resolutionButton' not found";
+  resolutionButton.onclick = resolute;
+  const cosButton = document.getElementById("cos-button");
+  if(!cosButton) throw "Element '#cosButton' not found";
+  cosButton.onclick = drawCos;
+  const axesButton = document.getElementById("axes-button");
+  if(!axesButton) throw "Element '#axesButton' not found";
+  axesButton.onclick = drawAxes;
+  // Update color based on input change
+  const colorPicker = document.getElementById("colorPicker");
+  colorPicker.addEventListener('change', () => {
+    window.gdc.strokeStyle = colorPicker.value;
+  });
+});
+function drawCos() {
+  const w = 2;
+  window.gdc.beginPath();
+  window.gdc.lineWidth = w;
+  window.gdc.lineTo(0,75 + 50 * Math.cos(0));
+  for(let i = 0; i < 300; i += w) {
+    window.gdc.lineTo(i,75 + 50 * Math.cos(i/10));
+  }
+  window.gdc.moveTo(29,15);
+  window.gdc.stroke();
+}
+function drawAxes() {
+  window.gdc.beginPath();
+  window.gdc.lineWidth = 2;
+  window.gdc.moveTo(150,0);
+  window.gdc.lineTo(150,150);
+  window.gdc.stroke();
+  window.gdc.moveTo(0,75);
+  window.gdc.lineTo(300,75);
+  window.gdc.stroke();
+}
+function resolute() {
+  window.figure.width = "600";
+  window.figure.height = "300";
+}
+function expand() {
+  window.figure.style.width = "600px";
+  window.figure.style.height = "300px";
+}
+function drawLine1() {
+  window.gdc.beginPath();
+  window.gdc.lineWidth = 2;
+  window.gdc.moveTo(15,15);
+  window.gdc.lineTo(285,135);
+  window.gdc.stroke();
+}
+function drawLine2() {
+  window.gdc.beginPath();
+  window.gdc.lineWidth = 2;
+  window.gdc.moveTo(285,15);
+  window.gdc.lineTo(15,135);
+  window.gdc.stroke();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadAssets();
+  window.location.hash = "";
+  window.onhashchange = coinChanged;
+  $("#clear-rates").click(clearRates);
+  let canvas = $("#rates-canvas")[0];
+  window.dcW = canvas.clientWidth;
+  window.dcH = canvas.clientHeight; 
+  canvas.width = window.dcW;
+  canvas.height = window.dcH;
+  window.dc = canvas.getContext('2d');
+});
+function clearRates() {
+  const dc = document.getElementById("rates-canvas").getContext('2d');
+  dc.clearRect(0, 0, window.dcW, window.dcH);
+  window.location.hash = "";
+}
+function coinChanged() {
+  //console.log(document.location.hash);
+  if(window.location.hash.length <= 1) return;
+  loadHistory(document.location.hash.substring(1));
+}
+function loadHistory(assetId) {
+  $.ajax({
+    method: "GET",
+    url: `https://api.coincap.io/v2/assets/${assetId}/history?interval=d1`
+  })
+  .done(j => {
+    const button = document.querySelector(`a[href="#${assetId}"]`);
+    let r = Math.floor(200 * Math.random());
+    let g = Math.floor(200 * Math.random());
+    let b = Math.floor(200 * Math.random());
+    let a = 0.8;
+    if(button)
+    {
+      button.style.backgroundColor = `rgba(${r},${g},${b},${a})`;
+    }
+    else{
+      console.log(`#${assetId}`)
+    }
+    let minRate = Number(j.data[0].priceUsd);
+    let maxRate = Number(j.data[0].priceUsd);
+    let minTime = j.data[0].time;
+    let maxTime = j.data[0].time;
+    for(let rec of j.data) {
+      let rate = Number(rec.priceUsd);
+      if(rate > maxRate) maxRate = rate;
+      if(rate < minRate) minRate = rate;
+      if(rec.time > maxTime) maxTime = rec.time;
+      if(rec.time < minTime) minTime = rec.time;
+    }
+    a = 1;
+    const dc = document.getElementById("rates-canvas").getContext('2d');
+    dc.beginPath();
+    dc.lineWidth = 1;
+    dc.strokeStyle = `rgba(${r},${g},${b},${a})`;
+    for(let rec of j.data) {
+      let rate = Number(rec.priceUsd);
+      let x = (rec.time - minTime) * window.dcW / (maxTime - minTime);
+      let y = window.dcH - 10 - (rate - minRate) * (window.dcH - 20) / (maxRate - minRate);
+      dc.lineTo(x, y);
+    }
+    dc.stroke();
+    a = 0.3;
+    dc.fillStyle = `rgba(${r},${g},${b},${a})`;
+    dc.lineTo(window.dcW,window.dcH);
+    dc.lineTo(0,window.dcH);
+    dc.fill();
+    a = 1;
+    dc.fillStyle = `rgba(${r},${g},${b},${a})`;
+    dc.fillText(Math.round(maxRate), window.dcW - 50, 10);
+    dc.fillText(Math.round(minRate), window.dcW - 50, window.dcH);
+  });
+}
+function loadAssets() {
+  $.ajax({
+    method: "GET",
+    url: "https://api.coincap.io/v2/assets?limit=7"
+  })
+  .done(function(j) {
+    let html = "";
+    for(let asset of j.data) {
+      html +=`<a href="#${asset.id}" class="btn btn-flat lighten-5 rate-button">${asset.name}</a>`;
+    }
+    document.getElementById("assets-block").innerHTML = html;
+  });
+}
--- LP-1/events.js
+++ LP-1/events.js
@@ -26,3 +26,116 @@ function div1Click(e){
   span.textContent = e.target;
   //console.log(e.target);
 }
+//////////////////DragEndDrop//////////////////
+//DragEndDrop  - реалізужться взаємодією кількома подіями - затисканням кнопки миши і відпускання кнопки миші.
+
+document.addEventListener('DOMContentLoaded' , ()=>{
+  //затискання миші - лише в середині елементів , які це дозволяють
+  for(let d of document.querySelectorAll('[data-type="draggable"]')) {
+    d.addEventListener('mousedown', onDownMouse);
+  }
+  //document.getElementById("draggable").addEventListener("mousedown",onDounMouse);
+  //рух та відпускання миші - блок з дозволенним перетягуванням, або весь документ
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+  window.dragetElement = null;// взаємодія між подіями забезпечується через (глобальне поле обькту window);
+  window.choice = {
+    'positive': [],
+    'negative': []
+  };
+});
+
+function onDownMouse(e){
+  window.dragetElement = e.target.closest('[data-type="draggable"]');
+  e.preventDefault();
+  //style.left - це те що прописанно у стилях. Якщо у стилях немає запису , то значення порожнє
+  // !!!Для визначення реальних розмірів та позицій , використовуются інші(не стильові поля)
+  console.log(window.dragetElement.offsetLeft,  window.dragetElement.offsetTop)
+  // треба зафіксувати поточні координати миші - вони будут новою точкою відліку для блоку.
+  window.dragetElement.tapX = e.pageX - window.dragetElement.offsetLeft;
+  window.dragetElement.tapY = e.pageY - window.dragetElement.offsetTop;
+  const container = window.dragetElement.closest(".dnd-container");
+  if(!container){
+      throw "Element #dnd-container not found";
+  }
+  window.dragetElement.maxX = container.clientWidth - window.dragetElement.offsetWidth;
+  window.dragetElement.startX = window.dragetElement.style.left;
+  window.dragetElement.startY = window.dragetElement.style.top;
+
+
+}
+
+function onMouseMove(e){
+  if(window.dragetElement){
+      let newX = e.pageX - window.dragetElement.tapX;
+      let newY = e.pageY - window.dragetElement.tapY;
+      if(newX<0 || newY<0 || newX> window.dragetElement.maxX){
+          return;
+      }
+      window.dragetElement.style.left = newX + 'px';
+      window.dragetElement.style.top = newY + 'px';
+  }
+}
+
+function onMouseUp(e){
+  if(window.dragetElement){
+      window.dragetElement.style.zIndex = -1;
+      const elements = document.elementsFromPoint(e.pageX, e.pageY);
+      let isTarget = false;
+      for(let element of elements){
+        if(element.classList.contains("dnd-target")){
+          isTarget = element;
+        }
+      }
+      window.dragetElement.style.zIndex = 1;
+
+      if(isTarget){
+          console.log("in target");
+          window.dragetElement.style.top = isTarget.offsetTop + isTarget.clientHeight / 2.0 - window.dragetElement.clientHeight / 2.0 + 'px';
+          if(isTarget.classList.contains("dnd-positive")) {
+            window.choice.positive.push(window.dragetElement);
+          }
+          else{
+            window.choice.negative.push(window.dragetElement);
+          }
+          let shift = 50;
+          for(let elem of window.choice.positive) {
+            elem.style.left = shift + 'px';
+            shift += 30;
+          }
+          shift = 400;
+          for(let elem of window.choice.negative){
+            elem.style.left = shift + 'px';
+            shift += 30;
+          }
+          // if(isAllDragged()) {
+          //   alert('Thx for your answers');
+          //   location.reload();
+          // }
+
+      }
+      else{
+        window.dragetElement.style.left = window.dragetElement.startX;
+        window.dragetElement.style.top = window.dragetElement.startY;
+      }
+      window.dragetElement = null;
+  }
+}
+function isAllDragged() {
+  let res = true;
+  for(let d of document.querySelectorAll('[data-type="draggable"]')) {
+    let rect = d.getBoundingClientRect();
+    const elements = document.elementsFromPoint(rect.x, rect.y);
+    let isTarget = false;
+    for(let element of elements) {
+      if(element.classList.contains("dnd-target")){
+        isTarget = true;
+      }
+    }
+    if(!isTarget){
+      res = false;
+    }
+    //console.log(d.getBoundingClientRect());
+  }
+  return res;
+}
